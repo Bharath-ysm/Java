b3d503f7019bc4535abbd615068c8c98ca60e244
Bharath <113431459+Bharath-ysm@users.noreply.github.com>
2024-01-13 15:13:02 +0530

    commiting simple java program code and sum of two numbers code

---
+++ Simple.java
@@ -0,0 +1,8 @@
+class Simple
+{
+    public static void main(String[] args)
+    {
+        //print welcome to the world of java programming
+        System.out.println("welcome to the world of java");
+    }
+}
---
+++ Sum.java
@@ -0,0 +1,11 @@
+ class Sum 
+{
+    public static void main(String[] args) 
+    {
+        int a=10,b=10;
+
+        int c = a+b;
+
+        System.out.println("sum of two numbers is:"+c);
+    }
+}
