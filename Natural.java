683c43749286ed253a2ca7aac7feda494b9e4227
Bharath <113431459+Bharath-ysm@users.noreply.github.com>
2024-01-13 17:15:58 +0530

    commiting Natural code

---
+++ Natural.java
@@ -0,0 +1,11 @@
+ class Natural
+{
+    public static void main(String[] args)
+    {
+        //printing the natural numbers from one to hundred
+        for(int i=0;i<=100;i++)
+        {
+            System.out.print(i+" ");
+        }
+    }
+}
