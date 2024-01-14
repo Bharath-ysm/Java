dc5760fbb65b368bcc15e162fbb89c875084a4e9
Bharath <113431459+Bharath-ysm@users.noreply.github.com>
2024-01-14 14:49:55 +0530

    commiting simple array code

---
+++ Array.java
@@ -0,0 +1,17 @@
+public class Array 
+{
+    public static void main(String[] args)
+    {
+        int[] arr=new int[3];
+
+        //inserting or adding the elements
+        arr[0]=1;
+        arr[1]=2;
+        arr[2]=3;
+
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.print(i+" ");
+        }
+    }    
+}
