b4399f3b6f8f440541a2d7b150ac0d4e55caab56
Bharath <113431459+Bharath-ysm@users.noreply.github.com>
2024-01-31 15:31:07 +0530

    commiting fully qualified name code

---
+++ Fully.java
@@ -0,0 +1,9 @@
+class Fully
+{
+	public static void main(String[] args)
+	{
+		//fully qualified name
+		java.util.Date d=new java.util.Date();
+		System.out.println("current date and time is:"+d);
+	}
+}
