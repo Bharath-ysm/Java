69f7a72940a0518e5ac6749c6d30e39c13956d86
Bharath <113431459+Bharath-ysm@users.noreply.github.com>
2024-01-31 14:54:51 +0530

    commiting range of datatype code

---
+++ Range.java
@@ -0,0 +1,14 @@
+class Range
+{
+	public static void main(String[] args)
+	{
+		//range of Byte 
+		System.out.println(Byte.MAX_VALUE);
+		System.out.println(Byte.MIN_VALUE);
+		
+		//range of integer
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(Integer.MIN_VALUE);
+		
+	}
+}
