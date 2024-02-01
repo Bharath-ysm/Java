5db9ee44dfd33150dc70f5997d857e6b41fa0be9
Bharath <113431459+Bharath-ysm@users.noreply.github.com>
2024-02-01 08:13:08 +0530

    commiting print statements code

---
+++ Test.java
@@ -0,0 +1,11 @@
+class Test
+{
+	public static void main(String[] args)
+	{
+		//display the output only in console
+		System.out.println("Hello world");
+		
+		//it displays the output in console as well as in physical file
+		System.err.println("Hello program");
+	}
+}
