feab3bc01f1412291079f962880a3269d9da03c9
Bharath <113431459+Bharath-ysm@users.noreply.github.com>
2024-03-26 11:07:21 +0530

    commiting simple array list code

---
+++ New.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class New
+{
+	public static void main(String[] args)
+	{
+		ArrayList<Integer> al=new ArrayList<Integer>();
+		
+		al.add(1);
+		
+		al.add(2);
+		
+		al.add(3);	
+
+	    System.out.println(al.remove(2));
+		
+		System.out.println(al);
+	}
+}
